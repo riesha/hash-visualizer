7fdd055f8f3e40e83d9545dd97230da7e53a2033
riesha <seleneia@outlook.jp>
2023-04-06 12:53:28 +0200

    cleaned some of the shit and noob code

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,51 @@
 #![feature(hasher_prefixfree_extras)]
 #![feature(is_some_with)]
-use std::{env, fs::File, hash::Hasher, io::Read};
+use std::{env, fs::File, io::Read};
 
 use bitvec::{prelude::Msb0, view::BitView};
-use fnv::FnvHasher;
-use image::{ImageBuffer, Rgb, RgbImage};
-use imageproc::drawing::{draw_text_mut, text_size};
-use integer_sqrt::IntegerSquareRoot;
-use ring::digest::{Context, Digest, SHA256};
-use rusttype::{Font, Scale};
 
+use image::{Rgb, RgbImage};
+
+#[allow(dead_code)]
 const PAYLOAD: &str = r#"hi every1 im new!!!!!!! *holds up spork* my name is katy but u can call me t3h PeNgU1N oF d00m!!!!!!!! lol…as u can see im very random!!!! thats why i came here, 2 meet random ppl like me ^_^… im 13 years old (im mature 4 my age tho!!) i like 2 watch invader zim w/ my girlfreind (im bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its SOOOO random!!!! shes random 2 of course but i want 2 meet more random ppl =) like they say the more the merrier!!!! lol…neways i hope 2 make alot of freinds here so give me lots of commentses!!!!
 DOOOOOMMMM!!!!!!!!!!!!!!!! <--- me bein random again ^_^ hehe…toodles!!!!!
 
 love and waffles,
 
 t3h PeNgU1N oF d00m."#;
-//  img.put_pixel(x, y, Rgb([255, 255, 255]));
-//  img.put_pixel(x, y, Rgb([0, 0, 0]));
-fn hash_sha256(str: &str) -> Digest
-{
-    let mut context = Context::new(&SHA256);
 
-    context.update(str.as_bytes());
-    context.finish()
-}
 enum MagicNumbers
 {
-    FNV1A_PRIME = 16777619,
-    FNV1A_BIAS  = 2166136261,
+    Fnv1aPrime = 16777619,
+    Fnv1aBias  = 2166136261,
 }
-
-#[inline(always)]
+// these are of course not really how you wanna do this but its just fine for what i wanted to do...
 fn fnv1a(data: &[u8]) -> Vec<u8>
 {
     let mut res = Vec::new();
-    let mut out = MagicNumbers::FNV1A_BIAS as u32;
+    let mut out = MagicNumbers::Fnv1aBias as u32;
     for byte in data
     {
         out ^= *byte as u32;
-        out = out.wrapping_mul(MagicNumbers::FNV1A_PRIME as u32);
+        out = out.wrapping_mul(MagicNumbers::Fnv1aPrime as u32);
         res.extend_from_slice(&out.to_le_bytes());
     }
     res
 }
-#[inline(always)]
+
 fn fnv1(data: &[u8]) -> Vec<u8>
 {
     let mut res = Vec::new();
-    let mut out = MagicNumbers::FNV1A_BIAS as u32;
+    let mut out = MagicNumbers::Fnv1aBias as u32;
     for byte in data
     {
-        out = out.wrapping_mul(MagicNumbers::FNV1A_PRIME as u32);
+        out = out.wrapping_mul(MagicNumbers::Fnv1aPrime as u32);
         out ^= *byte as u32;
         res.extend_from_slice(&out.to_le_bytes());
     }
     res
 }
+
 fn main()
 {
     let args: Vec<String> = env::args().collect();
@@ -70,6 +59,7 @@ fn main()
         eprintln!("Wrong arguments!\nUsage: bin file|string <filepath|inputtext>");
         return;
     }
+
     let mode = mode.unwrap();
     let input = input.unwrap();
     let data = match mode.as_str()
@@ -87,12 +77,15 @@ fn main()
             panic!("invalid input")
         }
     };
+
     let hash = fnv1a(&data);
+
     println!(
         "string entropy: {} hashed entropy: {}",
         shannon_entropy(&data),
         shannon_entropy(&hash)
     );
+
     let bits = hash.view_bits::<Msb0>();
     let dimension = (bits.len() as f64).sqrt() as u32;
 
